171ed5365b9b52c978b56330ea34e714cccfb08e
VinodCGI <kvinodk4444@gmail.com>
2017-05-04 23:55:31 +0530

    Add files via upload

---
+++ 6.3app.js
@@ -0,0 +1,17 @@
+var n =1000;
+var sum=0; // var that holds sum in for loop
+var Sum=0; // var that holds sum in while loop
+for(var x=0; x<=n;x++){
+	sum+=x;
+}
+console.log("Using FOR loop :"+sum);
+
+var i=0;
+
+while(i<=1000){
+	Sum+=i;
+	i++;
+}
+console.log("Using WHILE loop:" +Sum);
+
+
